28217d7934e4acad5e4cd8f04dfe35de1fc1059d
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-11 20:48:18 +0100

    Create Cart.svelte

---
+++ src/pages/Cart.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Product from "~/sections/cart/Product.svelte";
+</script>
+
+<section>
+  <header>
+    <h1>Your shopping cart</h1>
+  </header>
+  {#each {length: 2} as product}
+    <div class="grid">
+      <Product 
+        quantity={1}
+        brand="Brand" 
+        product="Oversize Blazer"
+        meta={{
+          color: 'black',
+          size: 36
+        }}
+      />
+    </div>
+  {:else}
+    <p>Could not find any products.</p>
+  {/each}
+</section>
+
+<style>
+  section{
+    padding: 30rem;
+    width: 900px;
+    height: 800px;
+    background: crimson;
+  }
+</style>
