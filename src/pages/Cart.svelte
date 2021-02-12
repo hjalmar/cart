581bcb7e0a020fafde12507b648ca62d0151a703
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 05:35:38 +0100

    Updating cart with accessibility labels. Does it matter if the dialog is to outmost element? or will this inner be accepted as a dialog? And also shipping away styling content

--- src/pages/Cart.svelte
+++ src/pages/Cart.svelte
@@ -4,27 +4,58 @@
   import { clear } from '~/components/Modal.svelte';
 </script>
 
-<section class="cart">
+<section class="cart" role="dialog" aria-labelledby="label-heading">
   <header>
-    <h1>Your shopping cart</h1>
+    <h1 id="label-heading">Your shopping cart</h1>
     <Button variant="teal" on:click={_ => clear()} aria-label="Close">Close</Button>
   </header>
   <div class="content-wrapper">
-    <div class="products">
-      <header></header>
-      {#each {length: 2} as product}
-        <Product 
-          quantity={1}
-          brand="Brand" 
-          product="Oversize Blazer"
-          meta={{
-            color: 'black',
-            size: 36
-          }}
-        />
-      {/each}
+    <div class="cart-content">
+      <div class="products">
+        <header></header>
+        {#each {length: 4} as product}
+          <Product 
+            quantity={1}
+            brand="Brand" 
+            product="Oversize Blazer"
+            meta={{
+              color: 'black',
+              size: 36
+            }}
+          />
+        {/each}
+      </div>
     </div>
-    <p>Could not find any products.</p>
+    
+    <div class="checkout-information">
+      <div class="discount">
+        <header>
+          <h3>10% discount on your order</h3>
+        </header>
+        <p>As a first time shopper you get discount on your first order.</p>
+      </div>
+  
+      <div class="total">
+  
+      </div>
+  
+      <div class="actions">
+        <Button slim>
+          <slot slot="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="160 208 80 128 160 48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
+          </slot>
+          Continue shopping
+        </Button>
+  
+        <Button variant="teal">
+          <slot slot="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><rect x="24" y="56" width="208" height="144" rx="8" stroke-width="16" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" fill="none"></rect><line x1="167.99414" y1="168" x2="199.99414" y2="168" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="119.99414" y1="168" x2="135.99414" y2="168" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="23.99414" y1="96.85228" x2="231.99412" y2="96.85228" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
+          </slot>
+          Checkout
+        </Button>
+      </div>
+    </div>
+
   </div>
 </section>
 
@@ -33,8 +64,10 @@
     padding: 30rem;
     width: 100%;
     max-width: var(--max-width);
-    height: 800px;
+    height: 100%;
+    max-height: calc(100vh - 60rem);
     background: var(--color-salmon);
+    overflow-y: hidden;
   }
 
   section > header{
@@ -49,7 +82,28 @@
   }
 
   section > .content-wrapper{
-    padding: 40rem;
+    display: grid;
+    grid-template-rows: auto auto;
     background-color: var(--color-light);
+    height: calc(100% - 80rem);
   }
+  
+  .discount{
+    padding: 40rem;
+    border-top: 2px solid var(--color-gray);
+  }
+
+  .cart-content{
+    padding: 40rem;
+    height: 100%;
+    overflow-y: scroll;
+  }
+
+  .actions{
+    border-top: 2px solid var(--color-gray);
+    display: flex;
+    justify-content: space-between;
+    padding: 40rem 50rem 40rem 35rem;
+  }
+  
 </style>
