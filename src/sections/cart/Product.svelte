240bf7f9bd7b31077d39aa4295712449372a22a6
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 04:40:59 +0100

    Styled cart product item according to deisign spec

--- src/sections/cart/Product.svelte
+++ src/sections/cart/Product.svelte
@@ -15,11 +15,13 @@
     </div>
 
     <div class="content">
-      <h3>{brand}</h3>
-      <h4>{product}</h4>
+      <header>
+        <h3>{brand}</h3>
+        <h4>{product}</h4>
+      </header>
       <p class="meta">
         <span class="color" style="--color: {meta.color}">{meta.color}</span>
-        <span>{meta.color}</span>, <span>{meta.size}</span>
+        <span>{meta.color}</span>, <span>size {meta.size}</span>
       </p>
 
       <div class="actions">
@@ -44,6 +46,16 @@
     display: grid;
     grid-template-columns: repeat(3, auto);
     width: 100%;
+    gap: 10rem;
+    align-items: center;
+  }
+  .product::after{
+    content: '';
+    height: 2px;
+    width: 100%;
+    margin: 25rem 0 35rem;
+    grid-column: 1 / 4;
+    background-color: var(--color-gray);
   }
   .product > *{
     display: flex;
@@ -55,4 +67,46 @@
   .product > *:last-child{
     justify-self: flex-end;
   }
+
+  .product-info{
+    display: grid;
+    gap: 20rem;
+    grid-template-columns: repeat(2, auto);
+  }
+
+  .image{
+    width: 200px;
+    height: 220px;
+    background: var(--color-gray);
+  }
+
+  .product-info .content{
+    padding: 20rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .meta{
+    display: flex;
+    align-items: center;
+  }
+  .meta > span::first-letter{
+    text-transform: capitalize;
+  }
+  .meta .color{
+    overflow: hidden;
+    margin-right: 10rem;
+    width: 30rem;
+    height: 30rem;
+    text-indent: -99999px;
+    background-color: var(--color);
+  }
+
+  .actions{
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    gap: 10rem;
+  }
+
 </style>
