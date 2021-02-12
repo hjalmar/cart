b023ed89aaa0216cb8356d5130f9943a8bd28151
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 03:16:24 +0100

    implemented button color variants

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,13 +1,26 @@
 <script>
-  export const variant = 'default';
+  export const variant = '';
 </script>
 
-<button on:click class:variant>
+<button on:click class="{variant}">
   <slot>Button placeholder</slot>
 </button>
 
 <style>
   button{
-    
+    border: none;
+    padding: 14rem 40rem;
+    background-color: var(--color-beige);
+    color: var(--color-dark);
+  }
+  .beige{
+    background-color: var(--color-beige);
+    color: var(--color-light);
+  }
+  .salmon{
+    background-color: var(--color-salmon);
+  }
+  .teal{
+    background-color: var(--color-teal);
   }
 </style>
