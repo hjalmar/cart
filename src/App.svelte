ea639cedb52dba9e06b8806529e9fbc1d782a16e
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 04:18:46 +0100

    Update App with Footer and Modal componets

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,23 @@
   import Router, { context } from 'svelte-standalone-router';
   
   // components 
-  import Cart from './pages/Cart.svelte';
-  import Masthead from './sections/masthead/Masthead.svelte';
+  import Index from '~/pages/Index.svelte';
+  import Cart from '~/pages/Cart.svelte';
+  import Masthead from '~/sections/masthead/Masthead.svelte';
+  import Footer from '~/sections/footer/Footer.svelte';
+  import Modal from '~/components/Modal.svelte';
 
   // routing for ease of expanding if time left
   const app = context({ initial: location.pathname });
   app.get('/', (req, res) => res.send(Cart));
 </script>
 
-
 <Masthead />
-
 <main>
   <Router />
 </main>
+<Footer />
+<Modal />
 
 <style>
   main{
