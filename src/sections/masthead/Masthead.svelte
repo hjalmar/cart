e12994827a4984cd3bf7245723ce7d6627316227
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 01:41:16 +0100

    Create Masthead.svelte

---
+++ src/sections/masthead/Masthead.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Topbar from './Topbar.svelte';
+  import Branding from '~/components/Branding.svelte'; 
+  import Navigate from './Navigation.svelte';
+</script>
+
+<header id="masthead">
+  <Topbar />
+  <div>
+    <Branding />
+    <Navigate />
+    <!-- <Cart /> -->
+  </div>
+</header>
