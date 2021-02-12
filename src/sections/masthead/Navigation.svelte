8904f00fae43efe9668ddf11476c0fd8dd7b2fab
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 01:41:26 +0100

    Create Navigation.svelte

---
+++ src/sections/masthead/Navigation.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { location } from 'svelte-standalone-router';
+  const links = [
+    {slug: '/', label: 'ACCESSOARER'},
+    {slug: '/', label: 'VÄSKOR'},
+    {slug: '/', label: 'SKOR'},
+    {slug: '/', label: 'VARUMÄRKEN'},
+    {slug: '/', label: 'OUTLET'}        
+  ];
+</script>
+
+<nav>
+  <ul>
+    {#each links as {slug, label}}
+      <li class:active={slug === $location}><a href="{slug}">{label}</a></li>
+    {/each}
+  </ul>
+</nav>
