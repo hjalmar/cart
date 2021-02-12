b4123cab6a0ea1d1841dd66ea02bf5bc1084160b
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 04:17:09 +0100

    Implementing modal push and clear to remove and add content to the modal. Also styled to overlap the page content

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,4 +1,29 @@
 <script context="module">
   import { writable } from 'svelte/store';
   const modal = writable();
+  export const push = (svelteComponent) => modal.set(svelteComponent);
+  export const clear = _ => modal.set(undefined);
 </script>
+
+{#if $modal}
+  {#key $modal}
+    <div class="modal">
+      hello
+      <svelte:component this={$modal} />
+    </div>
+  {/key}
+{/if}
+
+<style>
+  .modal{
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, .8);
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
