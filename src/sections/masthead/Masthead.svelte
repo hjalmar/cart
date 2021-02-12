eb197346372b15aaa6e0b83ee8d0583a2c4a2eb4
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-02-12 03:24:12 +0100

    Styled masthead according to design spec and replaced placeholder content with real content

--- src/sections/masthead/Masthead.svelte
+++ src/sections/masthead/Masthead.svelte
@@ -2,13 +2,37 @@
   import Topbar from './Topbar.svelte';
   import Branding from '~/components/Branding.svelte'; 
   import Navigate from './Navigation.svelte';
+  import CartButton from './CartButton.svelte';
 </script>
 
 <header id="masthead">
   <Topbar />
-  <div>
-    <Branding />
+  <div class="navigation max-width">
+    <div class="branding">
+      <Branding />
+    </div>
     <Navigate />
-    <!-- <Cart /> -->
+    <CartButton />
   </div>
 </header>
+
+
+<style>
+  #masthead{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, .12);
+  }
+  .branding{
+    max-width: 120px;
+  }
+  .navigation{
+    display: flex;
+    align-self: center;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20rem 0;
+  }
+</style>
